b09cd30bf1a76c1a11cedd823b4aa0f1bd5968f4
Your Name <you@example.com>
2021-03-29 20:40:16 -0500

    Finished bonus mission

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+  crew: string[] = ["Joseph Quinn", "Lynn West", "Katie Cope"];
   constructor() { }
 
   ngOnInit() {
